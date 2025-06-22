36664ac7ab33631e03912bf7473754e9d9839166
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-23 00:25:01 +0300

    feat: add slider logic (adjust number of squares)

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const container = document.querySelector(".container");
+const amountOfSquares = document.querySelector(".slider");
 
-const createGrid = () => {
-    for(i=0;i<16;i++)
+
+
+const createGrid = (amountOfSquares) => {
+    container.innerHTML = " ";
+    console.log(amountOfSquares)
+    for(i=0;i<amountOfSquares;i++)
     {
         const line = document.createElement("div");
         line.classList.add("line");
-        for(j=0;j<16;j++)
+        for(j=0;j<amountOfSquares;j++)
         {
             const square = document.createElement("div");
+            square.style.width = square.style.height = `${700/amountOfSquares}px`; 
             square.addEventListener("mouseenter", () => colorSquare(square));
             square.classList.add("square");
             line.appendChild(square);
@@ -22,4 +28,7 @@ const colorSquare = (square) => {
 }
 
 
-createGrid();
+
+createGrid(amountOfSquares.value);
+
+amountOfSquares.addEventListener("change", () => {createGrid(amountOfSquares.value)});
